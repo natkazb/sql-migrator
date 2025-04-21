641af55960ef8e21cc4437f635c80e1aa50fdf94
n.zbiranik <n.zbiranik@cdek.ru>
2025-04-21 12:37:13 +0700

    add logger_test

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/lib/pq v1.10.9
 	github.com/spf13/cobra v1.9.1
+	github.com/stretchr/testify v1.10.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -14,5 +15,4 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
 )
--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -1,7 +1,76 @@
 package logger
 
-import "testing"
+import (
+	"bytes"
+	"os"
+	"testing"
 
-func TestLogger(_ *testing.T) {
-	// TODO
+	"github.com/stretchr/testify/require"
+)
+
+func TestLogger(t *testing.T) {
+	tests := []struct {
+		level       string
+		method      func(*Logger, string)
+		message     string
+		expectedLog bool
+	}{
+		{"DEBUG", (*Logger).Debug, "Debug message", true},
+		{"DEBUG", (*Logger).Info, "Info message", true},
+		{"DEBUG", (*Logger).Warn, "Warn message", true},
+		{"DEBUG", (*Logger).Error, "Error message", true},
+		{"INFO", (*Logger).Debug, "Debug message", false},
+		{"INFO", (*Logger).Info, "Info message", true},
+		{"INFO", (*Logger).Warn, "Warn message", true},
+		{"INFO", (*Logger).Error, "Error message", true},
+		{"WARN", (*Logger).Debug, "Debug message", false},
+		{"WARN", (*Logger).Info, "Info message", false},
+		{"WARN", (*Logger).Warn, "Warn message", true},
+		{"WARN", (*Logger).Error, "Error message", true},
+		{"ERROR", (*Logger).Debug, "Debug message", false},
+		{"ERROR", (*Logger).Info, "Info message", false},
+		{"ERROR", (*Logger).Warn, "Warn message", false},
+		{"ERROR", (*Logger).Error, "Error message", true},
+	}
+
+	for _, test := range tests {
+		t.Run(test.level+"_"+test.message, func(t *testing.T) {
+			// Сохранить оригинальный os.Stdout
+			originalStdout := os.Stdout
+			// Создать pipe
+			r, w, err := os.Pipe()
+			require.NoError(t, err, "Ошибка создания pipe: %v\n", err)
+			if err != nil {
+				return
+			}
+			// Перенаправить os.Stdout на запись в pipe
+			os.Stdout = w
+
+			// Create logger and call the method
+			logger := New(test.level)
+			test.method(logger, test.message)
+
+			// Закрыть запись в pipe
+			w.Close()
+
+			var buf bytes.Buffer
+			_, err = buf.ReadFrom(r)
+			require.NoError(t, err, "Ошибка чтения из pipe: %v\n", err)
+			if err != nil {
+				return
+			}
+
+			// Восстановить оригинальный os.Stdout
+			os.Stdout = originalStdout
+
+			// Check if the message was logged
+			output := buf.String()
+			if test.expectedLog {
+				require.Contains(t, output, test.message, "Expected log message '%s', but it was not logged", test.message)
+			}
+			if !test.expectedLog {
+				require.NotContains(t, output, test.message, "Did not expect log message '%s', but it was logged", test.message)
+			}
+		})
+	}
 }
